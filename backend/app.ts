168863dc3f83e743d1e223e71459e6098a3aa9d4
Koo <kooroshr@hotmail.com>
2024-05-16 14:29:45 +0100

    added basic router

--- backend/app.ts
+++ backend/app.ts
@@ -1,9 +1,6 @@
 import express from 'express'
 export const app = express();
-// const apiRouter = require('./routes/api-router');
+import router from './routes/api-router'
 import cors from 'cors'
 
-app.get('/', (req: express.Request, res: express.Response) => {
-    res.status(200)
-        .send('Hello, welcome to the server!')
-})
+app.use('/', router)
