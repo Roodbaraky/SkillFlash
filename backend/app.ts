11890c0a834a6b306e9395aa93cd4492f64eec1f
anaggomes <157487724+anaggomes@users.noreply.github.com>
2024-05-16 17:34:43 +0100

    fixes axios post request from log in page, installs CORS

--- backend/app.ts
+++ backend/app.ts
@@ -1,7 +1,9 @@
-import express from 'express'
+import express from "express";
 export const app = express();
-import router from './routes/api-router'
-import cors from 'cors'
-app.use(express.json())
+import router from "./routes/api-router";
+import cors from "cors";
 
-app.use('/', router)
+app.use(cors());
+app.use(express.json());
+
+app.use("/", router);
